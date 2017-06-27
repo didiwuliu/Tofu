3fa88aa55d64fc6ddf28d774cda7704d3f31b5bf
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-06-27 15:18:00 +0800

    upgrade to suit for new swift

--- Tofu/Password.swift
+++ Tofu/Password.swift
@@ -1,32 +1,32 @@
 import Foundation
 
 final class Password {
-  var algorithm: Algorithm = .SHA1
+  var algorithm: Algorithm = .sha1
   var counter = 0
   var digits = 6
   var period = 30
-  var secret = NSData()
+  var secret = Data()
   var timeBased = false
 
-  func valueForDate(date: NSDate) -> String {
+  func valueForDate(_ date: Date) -> String {
     let counter = timeBased ?
       Int64(date.timeIntervalSince1970) / Int64(period) : Int64(self.counter)
     var input = counter.bigEndian
-    let digest = UnsafeMutablePointer<UInt8>.alloc(algorithm.digestLength)
-    defer { digest.destroy() }
-    CCHmac(algorithm.hmacAlgorithm, secret.bytes, secret.length, &input, sizeofValue(input), digest)
+    let digest = UnsafeMutablePointer<UInt8>.allocate(capacity: algorithm.digestLength)
+    defer { digest.deinitialize() }
+    CCHmac(algorithm.hmacAlgorithm, (secret as NSData).bytes, secret.count, &input, MemoryLayout.size(ofValue: input), digest)
     let bytes = UnsafePointer<UInt8>(digest)
     let offset = bytes[algorithm.digestLength - 1] & 0x0f
-    let number = UInt32(bigEndian: UnsafePointer<UInt32>(bytes + Int(offset)).memory) & 0x7fffffff
+    let number = UInt32(bigEndian: UnsafeRawPointer(bytes + Int(offset)).load(as: UInt32.self)) & 0x7fffffff
     return String(format: "%0\(digits)d", number % UInt32(pow(10, Float(digits))))
   }
 
-  func progressForDate(date: NSDate) -> Double {
+  func progressForDate(_ date: Date) -> Double {
     return timeIntervalRemainingForDate(date) / Double(period)
   }
 
-  func timeIntervalRemainingForDate(date: NSDate) -> Double {
+  func timeIntervalRemainingForDate(_ date: Date) -> Double {
     let period = Double(self.period)
-    return period - (date.timeIntervalSince1970 % period)
+    return period - (date.timeIntervalSince1970.truncatingRemainder(dividingBy: period))
   }
 }
